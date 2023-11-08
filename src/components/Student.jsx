cc27f334ec820628af856aef693ae25af5f74073
Ansel Rognlie <ansel.rognlie@gmail.com>
2023-11-08 15:06:21 -0800

    styles and presence toggle feature

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,12 +1,26 @@
 import "./Student.css";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const Student = (props) => {
+  const [isPresent, setIsPresent] = useState(false);
+
+  const togglePresence = () => {
+    setIsPresent(!isPresent);
+  };
+
+  const nameColor = isPresent ? "green" : "red";
+
   return (
-    <ul>
-      <li>Nickname: {props.name}</li>
-      <li>Email: {props.email}</li>
-    </ul>
+    <div>
+      <ul>
+        <li className={nameColor}>Nickname: {props.name}</li>
+        <li>Email: {props.email}</li>
+      </ul>
+      <button onClick={togglePresence}>
+        Toggle if {props.name} is present
+      </button>
+    </div>
   );
 };
 
