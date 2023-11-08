deaa09ad4d2616ad244f0db9910f1b30d704f1d5
Ansel Rognlie <ansel.rognlie@gmail.com>
2023-11-08 14:54:57 -0800

    add Student component

---
+++ src/components/Student.jsx
@@ -0,0 +1,12 @@
+import "./Student.css";
+
+const Student = (props) => {
+  return (
+    <ul>
+      <li>Nickname: {props.name}</li>
+      <li>Email: {props.email}</li>
+    </ul>
+  );
+};
+
+export default Student;
